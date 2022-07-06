f6f1ac9e9e93cd362789f054cf6f0482d6dd2653
Lyn <lynfinman@gmail.com>
2022-07-05 22:54:22 -0400

    commit tree 2e4c2195ab402bb406c6624b67575eb50efb9f59

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,8 @@ class Show < ActiveRecord::Base
   belongs_to :network
   has_many :characters
   has_many :actors, through: :characters
+
+  def actors_list
+    characters.map { |character| "#{character.actor.first_name} #{character.actor.last_name}" }
+  end
 end
